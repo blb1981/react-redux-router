2efc60c80aba8d6928a2297e63127e0b01fae1cb
Brandon <brandonbland90@gmail.com>
2020-12-11 16:02:47 -0500

    Added date picker component, working on addNote functionality

--- src/components/NoteForm.js
+++ src/components/NoteForm.js
@@ -1,10 +1,21 @@
-import React, { useState } from 'react'
+import React from 'react'
 import { connect } from 'react-redux'
+import moment from 'moment'
+import { SingleDatePicker } from 'react-dates'
+import 'react-dates/lib/css/_datepicker.css'
+// import {MuiPickersUtilsProvider, KeyboardDatePicker } from "@material-ui/pickers";
+
+const now = moment()
+console.log(now.format('lll'))
+// console.log(now.format('MMM Do, YYYY - H:m a Z'))
 
 class NoteForm extends React.Component {
   state = {
     noteTitle: '',
     noteBody: '',
+    createdAt: moment(),
+    dateFocused: false,
+    errors: '',
   }
   handleTitleChange = (e) => {
     const noteTitle = e.target.value
@@ -14,25 +25,63 @@ class NoteForm extends React.Component {
     const noteBody = e.target.value
     this.setState(() => ({ noteBody }))
   }
+  onDateChange = (createdAt) => {
+    this.setState(() => ({ createdAt }))
+  }
+  onFocusChange = ({ focused }) => {
+    this.setState(() => ({ dateFocused: focused }))
+  }
+  onSubmit = (e) => {
+    e.preventDefault()
+    if (!this.state.noteTitle || !this.state.noteBody) {
+      this.setState(() => ({
+        errors: 'Please fill out Title and Body sections',
+      }))
+    } else {
+      this.setState(() => ({ errors: '' }))
+      const note = {
+        noteTitle: this.state.noteTitle,
+        noteBody: this.state.noteBody,
+        createdAt: this.state.createdAt.format(),
+      }
+      console.log(note)
+      // TODO: dispatch call to add note
+    }
+    console.log(this.state.errors)
+  }
   render() {
     return (
-      <form>
-        <input
-          type="text"
-          placeholder="Title..."
-          autoFocus
-          value={this.state.noteTitle}
-          onChange={this.handleTitleChange}
-        />
-        <br />
-        <textarea
-          placeholder="Note text..."
-          value={this.state.noteBody}
-          onChange={this.handleBodyChange}
-        ></textarea>
-        <br />
-        <button type="submit">Add</button>
-      </form>
+      <div>
+        {this.state.errors && (
+          <div style={{ color: 'red' }}>{this.state.errors}</div>
+        )}
+        <form onSubmit={this.onSubmit}>
+          <input
+            type="text"
+            placeholder="Title..."
+            autoFocus
+            value={this.state.noteTitle}
+            onChange={this.handleTitleChange}
+          />
+          <br />
+          <textarea
+            placeholder="Note text..."
+            value={this.state.noteBody}
+            onChange={this.handleBodyChange}
+          ></textarea>
+          <br />
+          <SingleDatePicker
+            date={this.state.createdAt}
+            onDateChange={this.onDateChange}
+            focused={this.state.dateFocused}
+            onFocusChange={this.onFocusChange}
+            numberOfMonths={1}
+            isOutsideRange={() => false}
+          />
+          <br />
+          <button type="submit">Add</button>
+        </form>
+      </div>
     )
   }
 }
